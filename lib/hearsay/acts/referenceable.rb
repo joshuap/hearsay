e57ee40a0280ecaf21ab5d177f1b273f48b3aa5a
Joshua Wood <josh@hintmedia.com>
2012-01-23 12:29:12 -0800

    track attribute name for referenceable objects

--- lib/hearsay/acts/referenceable.rb
+++ lib/hearsay/acts/referenceable.rb
@@ -9,9 +9,22 @@ module Hearsay
         def referenced_by(source_name, options = {})
           send(:include, Hearsay::Acts::Referenceable::InstanceMethods) unless self.included_modules.include?(Hearsay::Acts::Referenceable::InstanceMethods)
           
+          attribute_name = options.delete(:method)
+          raise ArgumentError, ":method is required for references" if attribute_name.blank?
+          
+          association_name = "#{source_name.to_s.singularize}_references".to_sym
+          
           class_eval do
-            has_many(:referenceable_referencers, :as => :referenceable, :class_name => 'Reference', :dependent => :destroy) unless self.respond_to?(:referenceable_referencers)
-            has_many source_name, :through => :referenceable_referencers, :source => :referencer, :source_type => (options[:class_name] || source_name.to_s.classify)
+            has_many(association_name,
+              :as => :referenceable,
+              :class_name => 'Reference',
+              :conditions => {:attribute_name => attribute_name},
+              :dependent => :destroy) unless self.respond_to?(association_name)
+
+            has_many source_name,
+              :through => association_name,
+              :source => :referencer,
+              :source_type => (options[:class_name] || source_name.to_s.classify)
           end
         end
       end
